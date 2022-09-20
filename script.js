a667044404fd3a1b22741a4b8f277039f8c59c9f
Balogunmba22 <Balogunmba22@gmail.com>
2022-09-20 11:58:30 +0100

    some updates

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
+const logoEl = document.querySelectorAll('.logo');
 
- const body = document.getElementsByTagName('body')[0];
-        body.style.backgroundColor = 'ghostwhite';
+const rgbRandNumber = num => Math.floor(Math.random() * num + 1);
 
-        // trigger this function every time the user scrolls
-        window.onscroll = function (event) {
-            let scroll = window.pageYOffset;
-            if (scroll < 300) {
-                body.style.backgroundColor = 'ghostwhite';
-            } else if (scroll >= 300 && scroll < 600) {
-                body.style.backgroundColor = '#f5f9f0';
-            } else if (scroll >= 600 && scroll < 1200) {
-                body.style.backgroundColor = 'linen'
-            } else if (scroll >= 1200 && scroll < 1800) {
-                body.style.backgroundColor = '#ebf9f4';
-            } else if (scroll >= 1800 && scroll < 3000) {
-                body.style.backgroundColor = '#f0f2f5';
-            } else {
-                body.style.backgroundColor = '#2c2d36';
-            }
-        }
+function randomColor() {
+   let color = `rgb(${rgbRandNumber(255)}, ${rgbRandNumber(255)}, ${rgbRandNumber(255)})`
+   return color
+} 
+
+for (const letter of logoEl) {
+
+   function logoColor() {
+      return letter.style.color = randomColor()
+
+   }
+
+   setInterval(logoColor, 1000)
+
+}
