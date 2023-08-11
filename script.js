1a8370aee0bd6f1eaca81fe5cf167e076140063f
Balogunmba22 <balogunmba22@gmail.com>
2023-08-11 04:29:22 +0100

    project refactored

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const titleArray = [
   "Frontend Developer",
   "Software Engineer",
-  "Technical Communicator",
+  "Technical Writer",
 ];
-const numberArray = ["0", "9", "0", "2", "5", "5", "8", "5", "4", "9", "2"];
 
 // Selecting Elements to work on
 const titleEl = document.querySelector(".titles");
-const numberEl = document.querySelector(".number");
 
 // Getting and setting titles randomnly into the title Element
 const randomNo = () => Math.floor(Math.random() * titleArray.length);
@@ -16,15 +14,3 @@ const setTitle = () => {
   titleEl.textContent = randomTitle();
 };
 setInterval(setTitle, 2000);
-
-// Displaying numbers into the number Element
-for (const num of numberArray) {
-  numberEl.innerHTML += `<small>${num}</small>`;
-}
-
-//  <div class="photo-container">
-//           <img src="/media/my_pic.png" alt="My Photo" />
-//           <figcaption class="titles">
-//             <!-- titles goes here -->
-//           </figcaption>
-//         </div>
